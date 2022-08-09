ec9ec36d7cea914d74cd47ccbc43a7dabed1580f
ethmore <cetinermehmetali@gmail.com>
2022-08-09 12:30:24 +0300

    first init

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require("cors")
+const path = require('path');
+
+app.use(express.json());
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+const port = 3001;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/views/home.html'));
+});
+
+app.get('/seller-register', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/views/sellerRegister.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ public/sellerRegister.js
@@ -0,0 +1,41 @@
+const form = document.getElementById("sellerRegisterForm");
+form.addEventListener("submit", submitSellerRegister);
+
+function submitSellerRegister(e) {
+    e.preventDefault();
+    const companyName = document.getElementById("companyName").value
+    const email = document.getElementById("eMail").value
+    const password = document.getElementById("password").value
+    const passwordAgain = document.getElementById("passwordAgain").value
+    const address = document.getElementById("address").value
+    const phoneNumber = document.getElementById("phoneNumber").value
+
+    const data = {
+        companyName,
+        email,
+        password,
+        passwordAgain,
+        address,
+        phoneNumber
+    }
+    
+    fetch("http://127.0.0.1:3002/sellerRegister", {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" }
+    }).then(response => response.json())
+    .then(data => {
+        if (data.message === "") { //Message will be changed
+            companyName = null
+            email = null
+            password = null
+            passwordAgain = null
+            address = null
+            phoneNumber = null
+
+        } else {
+        }
+    })
+    .catch((err) => ("Error occured", err));
+}
+
