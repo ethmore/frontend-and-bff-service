b9aa5ce934b27ceabf3fd817190e13c5603a4473
ethmore <cetinermehmetali@gmail.com>
2022-08-19 18:28:59 +0300

    Added user and seller login

---
+++ public/scripts/userLogin.js
@@ -0,0 +1,26 @@
+const form = document.getElementById("userLogin");
+form.addEventListener("submit", submitSellerLogin);
+
+function submitSellerLogin(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email")
+    const password = document.getElementById("password")
+
+    const data = {
+        email: email.value,
+        password: password.value
+    }
+
+    fetch("http://127.0.0.1:3002/userLogin", {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" }
+    }).then(response => response.json())
+        .then(data => {
+            if (data.message === "OK") {
+                document.cookie = "token=" + data.token + "; SameSite=None; Secure"
+                window.location.href = "/profile"
+            }
+        })
+}
