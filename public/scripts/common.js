6451f985331872afdb8c2c2929fb532503205da0
ethmore <cetinermehmetali@gmail.com>
2022-08-21 17:48:03 +0300

    Changes for 'common' features, like navbar

---
+++ public/scripts/common.js
@@ -0,0 +1,210 @@
+document.addEventListener("DOMContentLoaded", init)
+
+function init(e) {
+    e.preventDefault();
+    validateToken();
+
+}
+
+function loadNavbar(tokenMail, loginType) {
+    const navBar = document.getElementById("navBar")
+
+    const logo = document.createElement("a")
+    logo.className = "logo"
+    logo.innerHTML = "eComm"
+    logo.href = "/"
+
+    const searchBar = document.createElement("input")
+    searchBar.className = "search-bar"
+    searchBar.type = "text"
+    searchBar.name = "searchBar"
+    searchBar.id = "searchBar"
+    searchBar.placeholder = "Search Product"
+
+    const form = document.createElement("form")
+    form.appendChild(searchBar)
+
+    const userDiv = document.createElement("div")
+    userDiv.className = "register"
+
+    if (loginType === "seller") {
+        if (tokenMail !== undefined) {
+            const dropdwn = sellerDropdown(tokenMail);
+            userDiv.appendChild(dropdwn)
+        } else {
+            eraseCookie("token")
+            window.location = "/seller-login"
+        }
+    } else if (loginType === "user") {
+        if (tokenMail !== undefined) {
+            const dropdown = userDropdown(tokenMail);
+            userDiv.appendChild(dropdown);
+        } else {
+            eraseCookie("token")
+            window.location = "/login"
+        }
+    } else {
+        const wrapper = loadUserSellerLoginRegister();
+        userDiv.appendChild(wrapper)
+    }
+
+    navBar.appendChild(logo)
+    navBar.appendChild(form)
+    navBar.appendChild(userDiv)
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
+
+function validateToken() {
+    const token = getCookie("token")
+
+    const data = {
+        token: token,
+    }
+
+    fetch("http://127.0.0.1:3002/profile", {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" }
+    }).then(response => response.json())
+        .then(data => {
+            if (data.message === "OK") {
+                loadNavbar(data.mail, data.type);
+            }
+            if (data.message === "loginNeeded") {
+                // window.location.href = "/seller-login"
+                loadNavbar(data.mail, data.type);
+            }
+        })
+}
+
+function loadUserSellerLoginRegister() {
+    const sellerRegister = document.createElement("a")
+    sellerRegister.href = "/seller-register"
+    sellerRegister.innerHTML = "Seller Register"
+
+    const sellerLogin = document.createElement("a")
+    sellerLogin.href = "/seller-login"
+    sellerLogin.innerHTML = "Seller Login"
+
+    const sellerDiv = document.createElement("div")
+    sellerDiv.className = "flex-column"
+    sellerDiv.appendChild(sellerRegister)
+    sellerDiv.appendChild(sellerLogin)
+
+    const userRegister = document.createElement("a")
+    userRegister.href = "/register"
+    userRegister.innerHTML = "User Register"
+
+    const userLogin = document.createElement("a")
+    userLogin.href = "/login"
+    userLogin.innerHTML = "User Login"
+
+    const userLogDiv = document.createElement("div")
+    userLogDiv.className = "flex-column"
+    userLogDiv.appendChild(userRegister)
+    userLogDiv.appendChild(userLogin)
+
+    const wrapper = document.createElement("div")
+    wrapper.appendChild(sellerDiv)
+    wrapper.appendChild(userLogDiv)
+    wrapper.className = "register"
+
+    return wrapper
+
+    // userDiv.appendChild(sellerDiv)
+    // userDiv.appendChild(userLogDiv)
+}
+
+function sellerDropdown(tokenMail) {
+    const dropdown = document.createElement("div");
+    dropdown.className = "dropdown";
+
+    const button = document.createElement("a")
+    button.className = "dropbtn"
+    button.innerHTML = "Seller Dashboard"
+    button.href = "/seller-dashboard"
+
+    const content = document.createElement("div")
+    content.className = "dropdown-content"
+
+    const sellerMail = document.createElement("p")
+    sellerMail.id = "email"
+    sellerMail.innerHTML = tokenMail
+
+    const addProductBtn = document.createElement("a")
+    addProductBtn.innerHTML = "Add Product"
+    addProductBtn.href = "/seller-add-product"
+
+    const logoutBtn = document.createElement("a")
+    logoutBtn.className = "logout-btn"
+    logoutBtn.innerHTML = "Logout"
+    logoutBtn.href = ""
+    logoutBtn.addEventListener("click", function(e) {
+        e.preventDefault();
+        eraseCookie("token")
+        window.location = "/"
+    })
+
+    content.appendChild(sellerMail);
+    content.appendChild(addProductBtn);
+    content.appendChild(logoutBtn);
+
+    dropdown.appendChild(button)
+    dropdown.appendChild(content)
+
+    return dropdown
+}
+
+function userDropdown(tokenMail) {
+    const dropdown = document.createElement("div");
+    dropdown.className = "dropdown";
+
+    const button = document.createElement("a")
+    button.className = "dropbtn"
+    button.innerHTML = "Account"
+    button.href = "/profile"
+
+    const content = document.createElement("div")
+    content.className = "dropdown-content"
+
+    const userMail = document.createElement("p")
+    userMail.id = "email"
+    userMail.innerHTML = tokenMail
+
+    const addProductBtn = document.createElement("a")
+    addProductBtn.innerHTML = "My addresses"
+    addProductBtn.href = "/user-addresses"
+
+    const logoutBtn = document.createElement("a")
+    logoutBtn.className = "logout-btn"
+    logoutBtn.innerHTML = "Logout"
+    logoutBtn.href = ""
+    logoutBtn.addEventListener("click", function(e) {
+        e.preventDefault();
+        eraseCookie("token")
+        window.location = "/"
+    })
+
+    content.appendChild(userMail);
+    content.appendChild(addProductBtn);
+    content.appendChild(logoutBtn);
+
+    dropdown.appendChild(button)
+    dropdown.appendChild(content)
+
+    return dropdown
+}
